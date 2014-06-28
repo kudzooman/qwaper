7984d6c6372b0ae870ff0b3a509b6e389a141f64
Chris Elwood <kudzooman@gmail.com>
2014-06-28 07:59:01 -0700

    created Post and Tag models with relations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628052730) do
+ActiveRecord::Schema.define(version: 20140628145614) do
+
+  create_table "posts", force: true do |t|
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["post_id"], name: "index_tags_on_post_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
