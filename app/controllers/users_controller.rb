6672e62a1919f835d08f00fc3929ec940bf4d68d
Chris Elwood <kudzooman@gmail.com>
2014-06-30 15:29:13 -0700

    created basic user view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  before_filter :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated"
+      redirect_to edit_user_registration_path(current_user)
+    else
+      render "devise/registrations/edit"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ resources :posts
 
 resources :tags
 
+resources :users
+
 get '/up-vote' => 'votes#up_vote', as: :up_vote
 get '/down-vote' => 'votes#down_vote', as: :down_vote
 
