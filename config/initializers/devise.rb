0b1e2b821f02b5a339f2aa81dbbd2b2b19148181
Chris Elwood <kudzooman@gmail.com>
2015-07-16 06:32:03 -0400

    changed heroku mailer account in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'app25190086@heroku.com'
+  config.mailer_sender = 'app26860440@heroku.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
